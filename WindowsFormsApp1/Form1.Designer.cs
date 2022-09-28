ca02a871cdff9f80300bdfbecbb15023348c88a0
NKostylev2 <57626704+NKostylev2@users.noreply.github.com>
2022-09-28 20:35:56 +0400

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.AnT = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.trackBar6 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "T=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "n=";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(183, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(356, 272);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // AnT
+            // 
+            this.AnT.AccumBits = ((byte)(0));
+            this.AnT.AutoCheckErrors = false;
+            this.AnT.AutoFinish = false;
+            this.AnT.AutoMakeCurrent = true;
+            this.AnT.AutoSwapBuffers = true;
+            this.AnT.BackColor = System.Drawing.Color.Black;
+            this.AnT.ColorBits = ((byte)(32));
+            this.AnT.DepthBits = ((byte)(16));
+            this.AnT.Location = new System.Drawing.Point(545, 12);
+            this.AnT.Name = "AnT";
+            this.AnT.Size = new System.Drawing.Size(276, 276);
+            this.AnT.StencilBits = ((byte)(0));
+            this.AnT.TabIndex = 14;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(31, 12);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 16;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(31, 44);
+            this.trackBar3.Maximum = 100;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 45);
+            this.trackBar3.TabIndex = 17;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(31, 76);
+            this.trackBar4.Maximum = 1000;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(104, 45);
+            this.trackBar4.TabIndex = 18;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.Location = new System.Drawing.Point(31, 108);
+            this.trackBar5.Maximum = 1000;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Size = new System.Drawing.Size(104, 45);
+            this.trackBar5.TabIndex = 19;
+            this.trackBar5.Scroll += new System.EventHandler(this.trackBar5_Scroll);
+            // 
+            // trackBar6
+            // 
+            this.trackBar6.Location = new System.Drawing.Point(31, 140);
+            this.trackBar6.Maximum = 1000;
+            this.trackBar6.Name = "trackBar6";
+            this.trackBar6.Size = new System.Drawing.Size(104, 45);
+            this.trackBar6.TabIndex = 20;
+            this.trackBar6.Scroll += new System.EventHandler(this.trackBar6_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "x = ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "y = ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 140);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "M = ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(142, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(35, 20);
+            this.textBox1.TabIndex = 24;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(35, 20);
+            this.textBox2.TabIndex = 25;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(142, 76);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(35, 20);
+            this.textBox3.TabIndex = 26;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(142, 108);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(35, 20);
+            this.textBox4.TabIndex = 27;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(142, 140);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(35, 20);
+            this.textBox5.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 292);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar6);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.AnT);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "s";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Tao.Platform.Windows.SimpleOpenGlControl AnT;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.TrackBar trackBar6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Windows.Forms;
+using Tao.FreeGlut;
+using Tao.OpenGl;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+
+        double ScreenW, ScreenH;
+
+        public double x, y, h, X1, Y1, f, g, k1, k2, k3, k4, l1, l2, l3, l4, M, T, num;
+        int n;
+
+        double[,] mass;
+        private void trackBar6_Scroll(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void trackBar5_Scroll(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            Draw();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            AnT.InitializeContexts();
+
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.ColumnCount = 3;
+            dataGridView1.Columns[0].Name = "N";
+            dataGridView1.Columns[1].Name = "X";
+            dataGridView1.Columns[2].Name = "Y";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            {
+                Glut.glutInit();
+                Glut.glutInitDisplayMode(Glut.GLUT_RGBA | Glut.GLUT_DOUBLE);
+                Gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+                Gl.glMatrixMode(Gl.GL_PROJECTION);
+                Gl.glLoadIdentity();
+
+                
+
+                Glu.gluOrtho2D(-15,15,-15,15);
+
+                Gl.glMatrixMode(Gl.GL_MODELVIEW);
+                Gl.glLoadIdentity();
+            }
+        }
+
+        double Function_g(double a, double b) => M * a - b - a * (Math.Pow(a, 2) + Math.Pow(b, 2));
+
+        double Function_f(double a, double b) => a + M * b - b * (Math.Pow(a, 2) + Math.Pow(b, 2));
+
+        public void Calculation()
+        {
+            dataGridView1.Rows.Clear();
+
+            T = (double)trackBar2.Value;
+            textBox1.Text = trackBar2.Value.ToString();
+            n = trackBar3.Value;
+            textBox2.Text = trackBar3.Value.ToString();
+            x = (double)trackBar4.Value / 100.0;
+            textBox3.Text = trackBar4.Value.ToString();
+            y = (double)trackBar5.Value / 100.0;
+            textBox4.Text = trackBar5.Value.ToString();
+            M = (double)trackBar6.Value / 100.0;
+            textBox5.Text = trackBar6.Value.ToString();
+
+            num = 0;
+            h = T / n;
+            mass = new double[(n + 1), 2];
+            mass[0, 0] = x;
+            mass[0, 1] = y;
+
+            for (int i = 1; i <= n; i++)
+            {
+                k1 = h * Function_f(mass[i - 1, 0], mass[i - 1, 1]);
+                l1 = h * Function_g(mass[i - 1, 0], mass[i - 1, 1]);
+
+                k2 = h * Function_f(mass[i - 1, 0] + k1 / 2, mass[i - 1, 1] + l1 / 2);
+                l2 = h * Function_g(mass[i - 1, 0] + k1 / 2, mass[i - 1, 1] + l1 / 2);
+
+                k3 = h * Function_f(mass[i - 1, 0] + k2 / 2, mass[i - 1, 1] + l2 / 2);
+                l3 = h * Function_g(mass[i - 1, 0] + k2 / 2, mass[i - 1, 1] + l2 / 2);
+
+                k4 = h * Function_f(mass[i - 1, 0] + k3, mass[i - 1, 1] + l3);
+                l4 = h * Function_g(mass[i - 1, 0] + k3, mass[i - 1, 1] + l3);
+
+                mass[i, 0] = mass[i - 1, 0] + (k1 + 2 * k2 + 2 * k3 + k4) / 6.0;
+                mass[i, 1] = mass[i - 1, 1] + (l1 + 2 * l2 + 2 * l3 + l4) / 6.0;
+
+                double fRez = (mass[i, 0]);
+                double gRez = (mass[i, 1]);
+                double xRez = (0 + i);
+
+                dataGridView1.Rows.Add(xRez, fRez, gRez);
+               
+                num++;
+            }
+        }
+        public void Draw()
+        {
+            Calculation();
+
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+    
+            Gl.glBegin(Gl.GL_LINE_STRIP);
+            Gl.glColor3f(1.0f, 0, 0);
+            
+            for (int i = 0; i < mass.GetLength(0); i += 2)
+            {
+                Gl.glVertex2d(mass[i, 0], mass[i, 1]);
+            }
+            Gl.glEnd();
+
+            Gl.glBegin(Gl.GL_LINES);
+            Gl.glColor3f(0, 0, 0);
+            Gl.glVertex2d(0, -15);
+            Gl.glVertex2d(0, 15);
+            Gl.glVertex2d(-15, 0);
+            Gl.glVertex2d(15, 0);
+            Gl.glEnd();
+            Gl.glFlush();
+
+            AnT.Invalidate();
+        }
+    }
+}
